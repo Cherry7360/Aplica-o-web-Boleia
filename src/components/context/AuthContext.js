74f7693e82e83597bdbb4f3b9aa9ad8788e6f1fd
Letícia Zego <101889483+Cherry7360@users.noreply.github.com>
2023-06-17 17:21:50 -0100

    AuthContext.js

--- src/components/context/AuthContext.js
+++ src/components/context/AuthContext.js
@@ -1,20 +1,20 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import { auth } from '../../firebase'
-import { signInWithPopup ,signOut,onAuthStateChanged,GoogleAuthProvider} from "firebase/auth";
+import { GoogleAuthProvider,signInWithPopup ,signOut,onAuthStateChanged} from "firebase/auth";
 
 const AuthContext = createContext()
 
  export function AuthContextProvider({ children }) {
-  
     const [user, setCurrentUser] = useState()
-   const [emailGoogle,setEmailGoogle]= useState()
+   
+    const [emailGoogle,setEmailGoogle]= useState()
     const [nameGoogle,setNameGoogle]= useState()
     const [porfilePicGoogle,setPorfilePicGoogle]= useState()
     const signInGoogle=()=>{
         const provider= new GoogleAuthProvider()
         signInWithPopup(auth,provider)
         .then((result)=>{
-         setNameGoogle(result.user.displayName)
+            setNameGoogle(result.user.displayName)
             setEmailGoogle(result.user.email)
             setPorfilePicGoogle(result.user.photoURL)
             console.log(result)
@@ -37,7 +37,10 @@ const AuthContext = createContext()
         <AuthContext.Provider value={{
             signInGoogle,
             LogOut,
-            user,emailGoogle,nameGoogle,porfilePicGoogle
+            user,
+            emailGoogle,
+            nameGoogle,
+            porfilePicGoogle
         }} >
             {children} 
         </AuthContext.Provider>
