6e3164f2c4984530e8498e922838702aa8d13201
Letícia Zego <101889483+Cherry7360@users.noreply.github.com>
2023-06-17 17:11:35 -0100

    Registrar.js

--- src/routes/Registrar.js
+++ src/routes/Registrar.js
@@ -3,9 +3,9 @@ import Signup from '../components/auth/Signup';
 function Registrar(){
     
     return(
-        <>
+        <div>
         <Signup/>
-        </>
+        </div>
     )
 }
 export default Registrar;
