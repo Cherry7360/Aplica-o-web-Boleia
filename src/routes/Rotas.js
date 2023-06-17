0531ce99e0ff7eb2648b8a7372d94884cfa107b6
Letícia Zego <101889483+Cherry7360@users.noreply.github.com>
2023-06-17 17:12:34 -0100

    Rotas.js

--- src/routes/Rotas.js
+++ src/routes/Rotas.js
@@ -1,7 +1,17 @@
+import './Rotas.css'
+import { Link} from 'react-router-dom';
 function Rotas(){
     return(
-        
-        <>
-        rotas </>
+        <div className='containerRotas'>
+            <div className='container-partilhar'>
+                <div className='partilharCarrona'>
+               <Link to='/forms'>
+                <button>Publicar Carona</button>
+                </Link> 
+            </div>
+            </div>
+            <div className="container-info2">
+            </div>
+        </div>
     )
 }export default Rotas
