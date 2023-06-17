7ad9652fec8d7417524d03921aaf62592e062161
Letícia Zego <101889483+Cherry7360@users.noreply.github.com>
2023-06-17 17:09:38 -0100

    Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,13 @@
-import {UserAuth} from'../components/context/AuthContext'
+//import {UserAuth} from'../components/context/AuthContext'
 function Home(){
-    const {emailGoogle}= UserAuth()
+   // const {user,emailGoogle,nameGoogle,porfilePicGoogle}= UserAuth()
     return(
         <>
             <h1>Home Page</h1>
           
-            <p>Email user:  {emailGoogle}</p>
+            not implemented yet
         
-       
         </>
     )
 }
 export default Home
-/*    <p>{nameGoogle}</p>
-         <img src={porfilePicGoogle} alt='porfile'/> */
